dc54819015d636737b47335b3304413fef040048
App Generator <51070104+app-generator@users.noreply.github.com>
2024-03-06 16:56:13 +0200

    Dummy

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.starter, name="index"),
     path("starter/", views.starter, name="starter"),
 
     path('load-stripe-products/', views.load_stripe_products, name="load_stripe_products"),
