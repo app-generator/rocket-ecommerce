1b2f7f4859dfa9ad7d8a6854789d5e6f36401f1b
hasib-helios <hasib@heliosinsider.com>
2024-03-10 10:20:43 +0600

    fix cart increment

--- apps/tables/urls.py
+++ apps/tables/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.datatables, name="datatables"),
     path('delete-product/<int:id>/', views.delete_product, name="delete_product"),
     path('update-product/<int:id>/', views.update_product, name="update_product"),
+    path('increment/<int:cart_id>/', views.increment_cart_item, name='cart_increment'),
+    path('decrement/<int:cart_id>/', views.decrement_cart_item, name='cart_decrement'),
 ]
