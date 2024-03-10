41763c2224b5abdb281bc9cc8d4058789e7097bb
hasib-helios <hasib@heliosinsider.com>
2024-03-10 16:04:09 +0600

    add product delete

--- home/urls.py
+++ home/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('load-stripe-products/', views.load_stripe_products, name="load_stripe_products"),
     path('create-related-product/<int:stripe_product_id>/', views.create_related_product, name="create_related_product"),
     path('edit-product/<int:product_id>/', views.edit_product, name="edit_product"),
+    path('delete-product/<int:id>/', views.delete_product, name="delete_local_product"),
     path('product/<int:product_id>/', views.product_details, name="product_details"),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name="add_to_cart"),
     path('cart/', views.cart_list, name="cart_list"),
