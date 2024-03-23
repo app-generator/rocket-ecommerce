9d6d9b8c52a525c9546fcc2bc3e1d4cb2f0d5a0e
Mominur Rahman <mominur.rahman@heliosinsider.com>
2024-03-23 13:06:30 +0600

    Add image cropper

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('load-stripe-products/', views.load_stripe_products, name="load_stripe_products"),
     path('create-related-product/<int:stripe_product_id>/', views.create_related_product, name="create_related_product"),
     path('edit-product/<int:product_id>/', views.edit_product, name="edit_product"),
+    path('edit-image/<int:product_id>/<str:img_name>/', views.edit_image, name="edit_image"),
     path('delete-product/<int:id>/', views.delete_local_product, name="delete_local_product"),
     path('delete-product/<int:local_product>/<int:stripe_product>/', views.delete_both_product, name="delete_both_product"),
     path('product/<str:slug>/', views.product_details, name="product_details"),
